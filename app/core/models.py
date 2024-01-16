f9ac58ba163408f4ae9b5ffa90b09656f0448fd2
arifhammad664 <arifhammad664@gmail.com>
2024-01-16 19:28:54 +0500

    configured custom user model and superuser with test coverages.

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,50 @@
-from django.db import models # noqa
+"""
+Database models
+"""
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    """Manager for users."""
+
+    def create_user(self, email, password=None, user_name=None, **extra_fields):
+        """Create, save and return a new user."""
+        if not user_name:
+            user_name = email.split('@')[0]
+
+        if not email:
+            raise ValueError('user must have an email address.')
+
+        user = self.model(email=self.normalize_email(email), user_name=user_name, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """Create and return and new super user."""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self.db)
+
+        return user
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """User in the system."""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    user_name = models.CharField(max_length=50, unique=True)
+    first_name = models.CharField(max_length=70)
+    last_name = models.CharField(max_length=70)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
